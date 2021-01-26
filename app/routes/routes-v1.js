edabf00454d1dcb3e8b610d76de9e9a64a6ff35a
miteshchohan <mitesh.chohan@gmail.com>
2021-01-26 15:28:50 +0000

    cleandup filters page

--- app/routes/routes-v1.js
+++ app/routes/routes-v1.js
@@ -303,16 +303,16 @@ function renderCheckboxIncState(selected, filter, name) {
 
 
 /**
-   * @description determine if an array contains one or more items from another array.
-   * @param {array} haystack the array to search.
-   * @param {array} arr the array providing items to check for in the haystack.
-   * @return {boolean} true|false if haystack contains at least one item from arr.
-   */
-  var findOne = function (haystack, arr) {
-    return arr.some(function (v) {
-        return haystack.indexOf(v) >= 0;
-    });
-  };
+ * @description determine if an array contains one or more items from another array.
+ * @param {array} haystack the array to search.
+ * @param {array} arr the array providing items to check for in the haystack.
+ * @return {boolean} true|false if haystack contains at least one item from arr.
+ */
+var findOne = function (haystack, arr) {
+  return arr.some(function (v) {
+      return haystack.indexOf(v) >= 0;
+  });
+};
 
 
 // router.get('/*', dataImport)
