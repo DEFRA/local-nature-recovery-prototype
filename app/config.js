6e5bbcbc9b8d80be1c4e756d770977d0d73176f8
Matt Fielding <matt@hippodigital.co.uk>
2021-01-29 18:13:10 +0000

    service name change

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Local Nature Recovery prototypes',
+  serviceName: 'Local Nature Recovery calculator',
 
   // Default port that prototype runs on
   port: '3000',
