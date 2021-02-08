f173a035a899ab0d7ef1cc55c096a6b2f6371739
Matt Fielding <matt@hippodigital.co.uk>
2021-02-08 12:26:59 +0000

    link up data file for ur1 participant

--- app/routes/routes-v1.js
+++ app/routes/routes-v1.js
@@ -592,7 +592,7 @@ function dataImport(req, res, next) {
     console.log('loading in data file')
     // pull in JSON data file
     delete req.session.data['import']
-    let grantsFile = 'grants-full-ur5.json'
+    let grantsFile = 'grants-full-ur1b.json'
     let path = 'app/data/'
     req.session.data['import'] = loadJSONFromFile(grantsFile, path)
 
