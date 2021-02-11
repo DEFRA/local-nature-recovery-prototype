ab8076d4657ea484eb17978ad2d570ad9c42c151
miteshchohan <mitesh.chohan@gmail.com>
2021-02-11 16:16:28 +0000

    route setup

--- app/routes/routes-v3.js
+++ app/routes/routes-v3.js
@@ -4,5 +4,14 @@ var jsonQuery = require('json-query')
 var nunjucks  = require('nunjucks')
 var env = nunjucks.configure()
 
+router.post('/options-choice/*/sbi-number', function (req, res) {
+    //console.log('inside sbi-number route');
+    res.redirect('sbi-land-confirm')
+})
+
+router.post('/options-choice/*/sbi-land-confirm', function (req, res) {
+    //console.log('inside sbi-number route');
+    res.redirect('sbi-land-confirm')
+})
 
 module.exports = router
