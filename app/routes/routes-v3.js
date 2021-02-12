f2b9c7dcda6d7508f524b7887ffb9d82b3ff88ac
miteshchohan <mitesh.chohan@gmail.com>
2021-02-12 10:48:27 +0000

    update to yes/no

--- app/routes/routes-v3.js
+++ app/routes/routes-v3.js
@@ -5,13 +5,12 @@ var nunjucks  = require('nunjucks')
 var env = nunjucks.configure()
 
 router.post('/options-choice/*/sbi-number', function (req, res) {
-    //console.log('inside sbi-number route');
     res.redirect('sbi-land-confirm')
 })
 
-router.post('/options-choice/*/sbi-land-confirm', function (req, res) {
-    //console.log('inside sbi-number route');
-    res.redirect('sbi-land-confirm')
-})
+// router.post('/options-choice/*/sbi-land-confirm', function (req, res) {
+//     console.log('inside sbi-number route');
+//     //res.redirect('sbi-land-confirm')
+// })
 
 module.exports = router
