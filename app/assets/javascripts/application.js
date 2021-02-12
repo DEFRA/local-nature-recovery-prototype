ce655fe10f49281ccfc2004d312e3c677e99528f
Matt Fielding <matt@hippodigital.co.uk>
2021-02-12 17:18:50 +0000

    updates to summary toggle feature

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,9 +8,48 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 
-  // toggle summeries for grant types
-  $('#summaryToggle').on('click', function (e) {
-    e.preventDefault();
-    $('.grantSummary').toggle();  
+  $('#summaryToggle').click(function () {
+    // The click handler will decide whether the toggle should "show"/"hide" and set the cookie.
+    var closed = $('.grantSummary').is(':hidden')
+    if (closed) {
+      $('.grantSummary').show()
+      $('#summaryToggle').html('<a href="#">Hide grant details</a><span class="govuk-accordion__icon hidden"></span>')
+    }
+    else {
+      $('.grantSummary').hide()
+      $('#summaryToggle').html('<a href="#">Show grant details</a><span class="govuk-accordion__icon"></span>')
+    }
+
+    setCookie('showSummary', closed, 365)
   });
-})
+
+  // The initial load event will try and pull the cookie to see if the toggle is "open"
+  var openToggle = getCookie('showSummary')
+  if (openToggle === 'true') {
+    $('.grantSummary').show()
+    $('#summaryToggle').html('<a href="#">Hide grant details</a><span class="govuk-accordion__icon hidden"></span>')
+  }
+  else {
+    $('.grantSummary').hide()
+    $('#summaryToggle').html('<a href="#">Show grant details</a><span class="govuk-accordion__icon"></span>')
+  }
+});
+
+function setCookie(c_name, value, exdays) {
+  var exdate = new Date()
+  exdate.setDate(exdate.getDate() + exdays)
+  var c_value = escape(value) + ((exdays == null) ? '' : '; expires=' + exdate.toUTCString())
+  document.cookie = c_name + '=' + c_value
+}
+
+function getCookie(c_name) {
+  var i, x, y, ARRcookies = document.cookie.split(';')
+  for (i = 0; i < ARRcookies.length; i++) {
+    x = ARRcookies[i].substr(0, ARRcookies[i].indexOf('='))
+    y = ARRcookies[i].substr(ARRcookies[i].indexOf('=') + 1)
+    x = x.replace(/^\s+|\s+$/g, '')
+    if (x == c_name) {
+      return unescape(y)
+    }
+  }
+}
