e1481fc5767cc6e510aa5c573b56edff2589e9e7
Matt Fielding <matt@hippodigital.co.uk>
2021-02-24 15:32:48 +0000

    add ur 16 data

--- app/routes/routes-v1.js
+++ app/routes/routes-v1.js
@@ -715,7 +715,7 @@ function dataImport(req, res, next) {
     console.log('loading in data file')
     // pull in JSON data file
     delete req.session.data['import']
-    let grantsFile = 'grants-full-ur15.json'
+    let grantsFile = 'grants-full-ur16.json'
     let path = 'app/data/'
     req.session.data['import'] = loadJSONFromFile(grantsFile, path)
 
